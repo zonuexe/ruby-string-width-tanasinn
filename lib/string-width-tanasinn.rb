9a7f4c047f8d9820d858e85aaf3c2364187cf8be
USAMI Kenta <tadsan@zonu.me>
2012-10-19 12:59:02 +0900

    init repo

---
+++ lib/string-width-tanasinn.rb
@@ -0,0 +1,9 @@
+require "string-width-tanasinn/version"
+
+module String
+  module Width
+    module Tanasinn
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/string-width-tanasinn/version.rb
@@ -0,0 +1,7 @@
+module String
+  module Width
+    module Tanasinn
+      VERSION = "0.0.1"
+    end
+  end
+end
